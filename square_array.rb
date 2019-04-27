a1d1adaba1ef1cac8c7d58c39b3378bd9b8690a2
hannah <hannah6530@gmail.com>
2019-04-27 17:34:48 -0400

    Done.

--- square_array.rb
+++ square_array.rb
@@ -2,9 +2,9 @@ numbers = [1,2,3]
 
 def square_array(array)
   # your code here
-  array.each {|i| i*i}
-end
-return array
+  new_array = []
+  array.each {|i| new_array.push(i*i)}
+  return new_array
 end
 
 
